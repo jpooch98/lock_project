a62f3385866f0010e97b83c5ef7a10e6a20b982a
James <45608681+jpooch98@users.noreply.github.com>
2023-11-03 15:45:33 -0400

    updated baseURL

--- src/Api/apiClient.js
+++ src/Api/apiClient.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:5000'
+        baseURL: 'https://lockproject.onrender.com'
     }
 );
